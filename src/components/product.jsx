2690a51072e5af63b2d885e5a596e3fe28d46a4f
Venmuhil, Selvakumar <Selvakumar.Venmuhil@sc.com>
2019-04-01 19:14:03 +0800

    [Selva] Added selected state for button

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -5,7 +5,8 @@ class Products extends Component {
   state = {
     name: this.props.name,
     imageUrl: this.props.imageUrl,
-    intro: this.props.intro
+    intro: this.props.intro,
+    isToggleOn: this.props.isToggleOn
   };
   render() {
     return (
@@ -13,12 +14,27 @@ class Products extends Component {
         <img src={this.state.imageUrl} alt="" />
         <div className="product-name clearfix">
           <h4 className="float-left">{this.state.name}</h4>
-          <button className="float-left btn btn-outline-white">Select</button>
+          <button className={this.getButtonClasses()} onClick={this.handleClick}>{this.renderButtonText()}</button>
         </div>
         <div className="product-intro">{this.state.intro}</div>
       </div>
     );
   }
+
+  handleClick = () => {
+    this.setState({ isToggleOn: !this.state.isToggleOn});
+  };
+
+  renderButtonText() {
+    if (this.state.isToggleOn) return 'Selected';
+    return 'Select';
+  }
+
+  getButtonClasses() {
+    let classes = "float-left btn ";
+    classes += this.state.isToggleOn ? "btn-fill-white" : "btn-outline-white";
+    return classes;
+  };
 }
 
 export default Products;
--- src/components/products.jsx
+++ src/components/products.jsx
@@ -9,6 +9,7 @@ class Products extends Component {
         id: 1,
         name: "Product Name 1",
         imageUrl: "https://picsum.photos/310/200?random",
+        isToggleOn: false,
         intro:
           "1 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
       },
@@ -16,6 +17,7 @@ class Products extends Component {
         id: 2,
         name: "Product Name 2",
         imageUrl: "https://picsum.photos/310/201?random",
+        isToggleOn: false,
         intro:
           "2 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
       },
@@ -23,6 +25,7 @@ class Products extends Component {
         id: 3,
         name: "Product Name 3",
         imageUrl: "https://picsum.photos/310/202?random",
+        isToggleOn: false,
         intro:
           "3 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
       },
@@ -30,6 +33,7 @@ class Products extends Component {
         id: 4,
         name: "Product Name 4",
         imageUrl: "https://picsum.photos/310/203?random",
+        isToggleOn: false,
         intro:
           "4 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
       }
@@ -45,6 +49,7 @@ class Products extends Component {
               key={product.id}
               name={product.name}
               imageUrl={product.imageUrl}
+              isToggleOn={product.isToggleOn}
               intro={product.intro}
             />
           ))}
